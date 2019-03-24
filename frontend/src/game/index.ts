cea20f2e255f4aca333787fda51a1ce094c6a7a6
aveyon <shiroxxs@googlemail.com>
2019-03-24 06:50:37 +0100

    kp

--- frontend/src/game/index.ts
+++ frontend/src/game/index.ts
@@ -1,23 +1,16 @@
-import { Mesh, MeshBasicMaterial, Object3D } from 'three';
-
 // please don't remove
-import { PerspectiveCamera, Scene, WebGLRenderer, Mesh, MeshBasicMaterial, Object3D, PlaneGeometry } from 'three';
-import { Player } from './player';
-import { Hurdle } from './Hurdle';
-import { Ebene } from './ebene';
-import { checkHitStatus } from './hit-status-check';
-import { GameView, GameData } from './game-view';
-import { Hurdle } from './Hurdle';
+import {Mesh, MeshBasicMaterial, Object3D, PlaneGeometry} from 'three';
+import {Player} from './player';
+import {Hurdle} from './Hurdle';
+import {Ebene} from './ebene';
+import {checkHitStatus} from './hit-status-check';
+import {GameData, GameView} from './game-view';
 
 // please don't remove
 if (module.hot)
     module.hot.dispose(() => location.reload());
 
 const gameView = new GameView(document.body);
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
 
 let mockData = [
     true, false, false, false, true, false, false, true, true, false, false,
@@ -45,9 +38,6 @@ background.position.z = -5;
 player.body.position.y = Ebene.Y_VALUE;
 player.body.position.x = -pufferEbene;
 
-camera.position.z = 18;
-camera.position.y = 7;
-
 // console.log(ebene.body.position);
 ebene.body.position.x = length / 2;
 
@@ -58,7 +48,7 @@ for (let i = 0; i <= length; i++) {
         let h = new Hurdle();
         // scene.add(hurdles[i].body);
         // console.log(h.body.position);
-        h.body.position.x = Math.round(- (length / 2));
+        h.body.position.x = Math.round(-(length / 2));
         h.body.position.x += i;
         h.body.position.y = 1.5;
         ebene.body.add(h.body);
@@ -66,11 +56,6 @@ for (let i = 0; i <= length; i++) {
     }
 
 }
-// console.log(hurdles);
-// add to the scene
-scene.add(player.body);
-scene.add(ebene.body);
-scene.add(background);
 
 let last;
 
@@ -97,10 +82,10 @@ function animate(timestamp) {
         gameView.moveLevel(diff / 500);
 
         const playerMeshMeterial = getMeshFromGroup(gameData.player.body);
-    if (result)
-        playerMeshMeterial.color.setRGB(1, 0.2, 0);
-    else
-        playerMeshMeterial.color.setHex(0xf2a2e8);
+        if (result)
+            playerMeshMeterial.color.setRGB(1, 0.2, 0);
+        else
+            playerMeshMeterial.color.setHex(0xf2a2e8);
 
         gameView.render();
     }
