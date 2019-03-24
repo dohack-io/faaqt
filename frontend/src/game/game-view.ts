82b15ba69055036595fd7213fe4500c4b3e39d7e
Feirell <Neirolf2030@web.de>
2019-03-24 06:34:37 +0100

    cleaned index in GameView

---
+++ frontend/src/game/game-view.ts
@@ -0,0 +1,80 @@
+import { Scene, PerspectiveCamera, WebGLRenderer } from "three";
+import { Ebene } from "./ebene";
+import { Player } from "./player";
+import { Hurdle } from "./Hurdle";
+
+export interface GameData {
+    hurdles: Hurdle[],
+    player: Player,
+    ebene: Ebene
+}
+
+export class GameView {
+    readonly scene;
+    readonly camera;
+    readonly renderer;
+
+    gameData: GameData = null;
+
+    constructor(private readonly wrapper: HTMLElement) {
+        this.scene = new Scene();
+        this.renderer = new WebGLRenderer();
+
+        const width = window.innerWidth - 300;
+        const height = window.innerHeight - 150;
+
+        this.camera = new PerspectiveCamera(45, width / height, 0.1, 100);
+        this.renderer.setSize(width, height);
+
+        wrapper.appendChild(this.renderer.domElement);
+    }
+
+    setLevel(data: boolean[]) {
+        const pufferEbene = 10;
+        const length = pufferEbene + data.length;
+
+        const ebene = new Ebene(length);
+        const player = new Player();
+
+        player.body.position.y = Ebene.Y_VALUE;
+        player.body.position.x = -pufferEbene;
+
+        this.camera.position.z = 18;
+        this.camera.position.y = 7;
+
+        ebene.body.position.x = length / 2;
+
+        const hurdles: Hurdle[] = [];
+
+        for (let i = 0; i <= length; i++) {
+
+            if (data[i]) {
+                const h = new Hurdle();
+
+                h.body.position.x = Math.round(- (length / 2)) + i;
+                h.body.position.y = 1.5;
+
+                ebene.body.add(h.body);
+                hurdles.push(h);
+            }
+
+        }
+
+        this.scene.add(player.body);
+        this.scene.add(ebene.body);
+
+        return this.gameData = { hurdles, player, ebene };
+    }
+
+    moveLevel(offset: number) {
+        this.gameData.ebene.body.position.x -= offset;
+    }
+
+    render() {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    jump() {
+        this.gameData.player.jump();
+    }
+}
--- frontend/src/game/index.ts
+++ frontend/src/game/index.ts
@@ -1,66 +1,14 @@
-// please don't remove
-import { PerspectiveCamera, Scene, WebGLRenderer, Mesh, MeshBasicMaterial, Object3D } from 'three';
-import { Player } from './player';
-import { Hurdle } from './Hurdle';
-import { Ebene } from './ebene';
-import { checkHitStatus } from './hit-status-check';
+import { Mesh, MeshBasicMaterial, Object3D } from 'three';
 
+import { checkHitStatus } from './hit-status-check';
+import { GameView, GameData } from './game-view';
+import { Hurdle } from './Hurdle';
+
+// please don't remove
 if (module.hot)
     module.hot.dispose(() => location.reload());
 
-let scene: Scene = new Scene();
-let camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
-let renderer = new WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
-
-let mockData = [
-    true, false, false, false, true, false, false, true, true, false, false,
-    false, false, false, false, true, false, false, true, true, false, false,
-    false, false, false, false, true, false, false, true, true, false, false,
-];
-
-//let geometry: BoxGeometry = new BoxGeometry(1, 1, 1);
-//let material: MeshBasicMaterial = new MeshBasicMaterial({color : 0x00ff00});
-//let cube : Mesh = new Mesh(geometry,material);
-let pufferEbene = 10;
-let length = pufferEbene + mockData.length;
-// Objekte erstellen
-let ebene: Ebene = new Ebene(length);
-let player: Player = new Player();
-
-// Initiale Koordinaten setzen
-
-player.body.position.y = Ebene.Y_VALUE;
-player.body.position.x = -pufferEbene;
-
-camera.position.z = 18;
-camera.position.y = 7;
-
-// console.log(ebene.body.position);
-ebene.body.position.x = length / 2;
-
-let hurdles: Hurdle[] = [];
-for (let i = 0; i <= length; i++) {
-
-    if (mockData[i]) {
-        let h = new Hurdle();
-        // scene.add(hurdles[i].body);
-        // console.log(h.body.position);
-        h.body.position.x = Math.round(- (length / 2));
-        h.body.position.x += i;
-        h.body.position.y = 1.5;
-        ebene.body.add(h.body);
-        hurdles.push(h);
-    }
-
-}
-// console.log(hurdles);
-// add to the scene
-scene.add(player.body);
-scene.add(ebene.body);
+const gameView = new GameView(document.body);
 
 let last;
 
@@ -70,40 +18,36 @@ const getMeshFromGroup = (group: Object3D) => {
     return playerBody.material as MeshBasicMaterial;
 };
 
+let gameData: null | GameData = null;
+
+gameData = gameView.setLevel([
+    false, true, true, false, true
+])
+
 function animate(timestamp) {
     requestAnimationFrame(animate);
     const diff = timestamp - last;
     last = timestamp;
 
-    // camera.position.x += diff / 500;
-    // player.body.position.x += diff / 500;
+    if (gameData !== null) {
+        const result = checkHitStatus(gameData.player.body, gameData.hurdles.map(h => h.body));
 
-    const result = checkHitStatus(player.body, hurdles.map(h => h.body));
-    const playerMeshMeterial = getMeshFromGroup(player.body);
+        gameView.moveLevel(diff / 500);
 
-    // camera.position.x += 0.01;
-    // player.body.position.x += 0.01 ;
-    ebene.body.position.x -= diff / 500;
-    if (result)
-        playerMeshMeterial.color.setRGB(1, 0.2, 0);
-    else
-        playerMeshMeterial.color.setRGB(0.2, 1, 0);
-
-
-    renderer.render(scene, camera);
+        const playerMeshMeterial = getMeshFromGroup(gameData.player.body);
+        if (result)
+            playerMeshMeterial.color.setRGB(1, 0.2, 0);
+        else
+            playerMeshMeterial.color.setRGB(0.2, 1, 0);
+    }
 };
-
 
 requestAnimationFrame(timestamp => {
     last = timestamp;
     requestAnimationFrame(animate);
 });
 
-// let movecamera = () => {
-//     for (let i = 0; i < 200; i++) {
-//     }
-// }
-
 window.addEventListener('keydown', (e) => {
-    player.jump();
+    if (e.key == 'SPACE')
+        gameView.jump();
 });
